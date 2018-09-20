7e4bfdbbaec122621925da8260e9c668592eb725
juanflorez <juanflorez@juanflorez-HP>
2018-09-19 21:42:00 -0500

    se crea clase conexion

---
+++ src/conexionmysqlcorba/conexion.java
@@ -0,0 +1,16 @@
+package conexionmysqlcorba;
+import java.sql.*;
+
+public class conexion{
+  public Connection Conexion;
+  
+  public Connection conectar(){
+      try {
+          Class.forName("com.mysql.jdbc.Driver");
+          Conexion=DriverManager.getConnection("jdbc:mysql://localhost/conexionmysqlcorba","root","");
+          System.out.println("Conexion establecida");
+      } catch (Exception e) {
+      }
+      return Conexion;
+  }
+}
