c655382abc5ae88f5a990cd9dea40dea391bfff0
juanflorez <juanflorez@juanflorez-HP>
2018-09-26 21:46:50 -0500

    se crea consultar

--- src/conexionmysqlcorba/Terceros.java
+++ src/conexionmysqlcorba/Terceros.java
@@ -6,7 +6,7 @@ import Terceros.*;
 
 
 public class Terceros extends TerceroPOA{
-    conexion obConec = new conexion();
+    conexion objConec = new conexion();
 
     @Override
     public boolean insertarTercero(String nombres, String apellidos, String telefono) {
@@ -25,7 +25,21 @@ public class Terceros extends TerceroPOA{
 
     @Override
     public String consultarTerceros(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      String resultado = "";
+        try {
+            String sqlConsultar = "Select* from terceros where id = "+ id;
+            objConec.conectar();
+            Statement st = objConec.Conexion.createStatement();
+            ResultSet rs = st.executeQuery(sqlConsultar);
+            while(rs.next()){
+            resultado += rs.getString(2)+"-"
+                    +rs.getString(3)+"-"
+                    +rs.getString(4);
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR: "+ e.getMessage());
+        }
+      return resultado;
     }
 
     @Override
